e5eea3a3b6c2b59cd140af8d528189b79ddcab97
thamvn <thamvn@blockchainlabs.asia>
2018-12-14 12:00:10 +0700

    init

---
+++ ByPassOdooConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ByPassOdooConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "quocltm@blockchainlabs.asia";
+            string password = "123password";
+            var odooService = new ByPassOdooService();
+            odooService.Login(username, password);
+            odooService.CheckIn();
+        }
+    }
+
+    class ByPassOdooService
+    {
+        CookieContainer cockieContainer = new CookieContainer();
+
+        public void Login(string username, string password)
+        {
+            var token = getLoginToken();
+            var url = "https://odoo.blockchainlabs.asia/web/login";
+            string loginParram =
+                        String.Format("csrf_token={0}&login={1}&password={2}&redirect=", token, System.Web.HttpUtility.UrlEncode(username), password);
+            string referel = $"https://odoo.blockchainlabs.asia/web/login";
+            byte[] loginParramByte = Encoding.UTF8.GetBytes(loginParram);
+            var response = RestClient.Send(url, "POST", cockieContainer, true, loginParramByte, referel);
+        }
+
+        private string getLoginToken()
+        {
+            string url = "https://odoo.blockchainlabs.asia/web/login";
+            var response = RestClient.Send(url, "GET", cockieContainer, true, null);
+            var regex = "(?<=csrf_token: \").{41}";
+            Regex rx = new Regex(regex, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var token = rx.Match(response.Result);
+            return token.Value;
+        }
+
+        public void CheckIn()
+        {
+            var url = "https://odoo.blockchainlabs.asia/web/dataset/call_kw/hr.employee/attendance_manual";
+            string loginParram1 = "{\"jsonrpc\":\"2.0\",\"method\":\"call\",\"params\":{\"args\":[[657],\"hr_attendance.hr_attendance_action_my_attendances\"],\"model\":\"hr.employee\",\"method\":\"attendance_manual\",\"kwargs\":{}},\"id\":" + getRpcId() + "}";
+
+            byte[] loginParramByte = Encoding.UTF8.GetBytes(loginParram1);
+            var response = RestClient.SendRpc(url, "POST", cockieContainer, true, loginParramByte);
+        }
+
+        private string getRpcId()
+        {
+            return (DateTime.Now.ToFileTime() % 999999999).ToString();
+        }
+    }
+}
---
+++ ByPassOdooConsole/RestClient.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace ByPassOdooConsole
+{
+    public class SendResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+        public string Result { get; set; }
+        public string SetCookie { get; set; }
+        public string ReponseUri { get; set; }
+        public string Location { get; set; }
+    }
+
+    public static class RestClient
+    {
+        private const string browserUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0";
+        private const string browseAccept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        private const string browseAcceptEncoding = "gzip, deflate";
+        private const string browseAcceptLanguage = "en-US,en;q=0.5";
+
+        public static SendResponse Send(string requestUri, string method, CookieContainer cookieContainer, 
+            bool isDecompression, byte[] data, string referer="")
+        {
+            //ServicePointManager.ServerCertificateValidationCallback =
+            //    delegate (Object obj, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            //    {
+            //        return true;
+            //    };
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                              | SecurityProtocolType.Tls11
+                                              | SecurityProtocolType.Tls12
+                                              | SecurityProtocolType.Ssl3;
+
+            HttpWebRequest request;
+            HttpWebResponse response = null;
+            SendResponse sendResponse = new SendResponse();
+            try
+            {
+                request = WebRequest.Create(requestUri) as HttpWebRequest;
+                //request.ProtocolVersion
+                request.Method = method;
+                request.UserAgent = browserUserAgent;
+
+                request.CookieContainer = cookieContainer;
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36";
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage,"en,vi;q=0.9,en-GB;q=0.8");
+                request.ContentType = "application/x-www-form-urlencoded";
+                //if (headerParams!=null)
+                //{
+                //    foreach (var headerParam in headerParams)
+                //    {
+                //        request.Headers.Add(headerParam.Key, headerParam.Value);
+                //    }
+                //}
+                if (!string.IsNullOrEmpty(referer))
+                {
+                    request.Referer = referer;
+                }
+                if (isDecompression)
+                {
+                    request.AutomaticDecompression = (System.Net.DecompressionMethods.GZip |
+                                                      System.Net.DecompressionMethods.Deflate);
+
+                }
+
+                if (data != null)
+                {
+                    // Set the content length in the request headers
+                    request.ContentLength = data.Length;
+                    // Write data  
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+
+                // Get response  
+                response = request.GetResponse() as HttpWebResponse;
+
+                if (request.HaveResponse == true && response != null)
+                {
+                    // Get the response stream
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        sendResponse.StatusCode = response.StatusCode;
+                        sendResponse.StatusDescription = response.StatusDescription;
+                        sendResponse.Result = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException wex)
+            {
+                // This exception will be raised if the server didn't return 200 - OK  
+                // Try to retrieve more information about the network error  
+                if (wex.Response != null)
+                {
+                    using (HttpWebResponse errorResponse = (HttpWebResponse)wex.Response)
+                    {
+                        sendResponse.StatusCode = errorResponse.StatusCode;
+                        sendResponse.StatusDescription = errorResponse.StatusDescription;
+                    }
+                }
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+
+            return sendResponse;
+        }
+        public static SendResponse SendRpc(string requestUri, string method, CookieContainer cookieContainer,
+            bool isDecompression, byte[] data, string referer = "")
+        {
+            //ServicePointManager.ServerCertificateValidationCallback =
+            //    delegate (Object obj, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            //    {
+            //        return true;
+            //    };
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                              | SecurityProtocolType.Tls11
+                                              | SecurityProtocolType.Tls12
+                                              | SecurityProtocolType.Ssl3;
+
+            HttpWebRequest request;
+            HttpWebResponse response = null;
+            SendResponse sendResponse = new SendResponse();
+            try
+            {
+                request = WebRequest.Create(requestUri) as HttpWebRequest;
+                //request.ProtocolVersion
+                request.Method = method;
+                request.UserAgent = browserUserAgent;
+
+                request.CookieContainer = cookieContainer;
+                request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36";
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "en,vi;q=0.9,en-GB;q=0.8");
+                request.ContentType = "application/json-rpc";
+                
+                //if (headerParams!=null)
+                //{
+                //    foreach (var headerParam in headerParams)
+                //    {
+                //        request.Headers.Add(headerParam.Key, headerParam.Value);
+                //    }
+                //}
+                if (!string.IsNullOrEmpty(referer))
+                {
+                    request.Referer = referer;
+                }
+                if (isDecompression)
+                {
+                    request.AutomaticDecompression = (System.Net.DecompressionMethods.GZip |
+                                                      System.Net.DecompressionMethods.Deflate);
+
+                }
+
+                if (data != null)
+                {
+                    // Set the content length in the request headers
+                    request.ContentLength = data.Length;
+                    // Write data  
+                    using (Stream stream = request.GetRequestStream())
+                    {
+                        stream.Write(data, 0, data.Length);
+                    }
+                }
+
+                // Get response  
+                response = request.GetResponse() as HttpWebResponse;
+
+                if (request.HaveResponse == true && response != null)
+                {
+                    // Get the response stream
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        sendResponse.StatusCode = response.StatusCode;
+                        sendResponse.StatusDescription = response.StatusDescription;
+                        sendResponse.Result = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException wex)
+            {
+                // This exception will be raised if the server didn't return 200 - OK  
+                // Try to retrieve more information about the network error  
+                if (wex.Response != null)
+                {
+                    using (HttpWebResponse errorResponse = (HttpWebResponse)wex.Response)
+                    {
+                        sendResponse.StatusCode = errorResponse.StatusCode;
+                        sendResponse.StatusDescription = errorResponse.StatusDescription;
+                    }
+                }
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                }
+            }
+
+            return sendResponse;
+        }
+    }
+}
